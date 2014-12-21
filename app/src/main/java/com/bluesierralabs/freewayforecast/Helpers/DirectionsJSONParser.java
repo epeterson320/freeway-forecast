93361fc8cafea6c76f5b7218c8fe841f6ac610f5
tvthatsme <timothy.n.vernon@gmail.com>
2014-12-21 16:53:58 -0500

    Placing markers for each hour of the route. Markers still off a little but its getting closer.

--- app/src/main/java/com/bluesierralabs/freewayforecast/Helpers/DirectionsJSONParser.java
+++ app/src/main/java/com/bluesierralabs/freewayforecast/Helpers/DirectionsJSONParser.java
@@ -1,6 +1,8 @@
 package com.bluesierralabs.freewayforecast.Helpers;
 
 
+import android.util.Log;
+
 import com.google.android.gms.maps.model.LatLng;
 
 import org.json.JSONArray;
@@ -17,6 +19,39 @@ import java.util.List;
 
 public class DirectionsJSONParser
 {
+    private List<LatLng> hourPoints;
+
+    public List<LatLng> getHourPoints() {
+        return hourPoints;
+    }
+
+    private LatLng getPointBetween(LatLng start, LatLng end, double percent)
+    {
+        Log.e("Getting point between locations, percent", "" + percent);
+
+        Double latitude;
+        Double longitude;
+        Double latDifference;
+        Double lngDifference;
+
+        if (start.latitude > end.latitude) {
+            latDifference = start.latitude - end.latitude;
+            latitude = start.latitude - (latDifference * percent);
+        } else {
+            latDifference = end.latitude - start.latitude;
+            latitude = start.latitude + (latDifference * percent);
+        }
+
+        if (start.longitude > end.longitude) {
+            lngDifference = start.longitude - end.longitude;
+            longitude = start.longitude - (lngDifference * percent);
+        } else {
+            lngDifference = end.longitude - start.longitude;
+            longitude = start.longitude + (lngDifference * percent);
+        }
+
+        return new LatLng(latitude, longitude);
+    }
 
     /** Receives a JSONObject and returns a list of lists containing latitude and longitude */
     public List<List<HashMap<String, String>>> parse(JSONObject jObject)
@@ -27,16 +62,29 @@ public class DirectionsJSONParser
         JSONArray jSteps = null;
         JSONObject jDistance = null;
         JSONObject jDuration = null;
+        JSONObject jDurationSeconds = null;
+
+        // Create an array list for the Latitude/Longitude points at the hour markers
+        hourPoints = new ArrayList<LatLng>();
+
+        int routeDuration;
+        int newRouteDuration;
 
         try
         {
             jRoutes = jObject.getJSONArray("routes");
+
+            Log.e("Found routes: ", "" + jRoutes.length());
+
             /** Traversing all routes */
             for (int i = 0; i < jRoutes.length(); i++)
             {
+                // Set the duration of the route to zero seconds
+                routeDuration = 0;
+
                 jLegs = ((JSONObject) jRoutes.get(i)).getJSONArray("legs");
                 List<HashMap<String, String>> path = new ArrayList<HashMap<String, String>>();
-                /** Traversing all legs */
+                /** Traversing all legs of the route */
                 for (int j = 0; j < jLegs.length(); j++)
                 {
                     /** Getting distance from the json data */
@@ -60,6 +108,7 @@ public class DirectionsJSONParser
                     /** Traversing all steps */
                     for (int k = 0; k < jSteps.length(); k++)
                     {
+                        // Get the polyline information
                         String polyline = "";
                         polyline = (String) ((JSONObject) ((JSONObject) jSteps.get(k)).get("polyline")).get("points");
                         List<LatLng> list = this.decodePoly(polyline);
@@ -72,8 +121,110 @@ public class DirectionsJSONParser
                             hm.put("lng", Double.toString((list.get(l)).longitude));
                             path.add(hm);
                         }
+
+                        // Get the duration of the step (in seconds) and add it to the trip duration
+                        int stepDuration;
+                        int stepModulus;
+
+                        stepDuration = ((JSONObject) ((JSONObject) jSteps.get(k)).get("duration")).getInt("value");
+
+                        // The step duration can do a couple things...
+                        // 1 - It could begin a new 'hour' of the trip
+                        // 2 - It could finish off an existing 'hour' of the trip
+                        // 3 - It could contain several 'hour's of the trip
+
+                        // The goal here is to pin an hour marker at every 3600 second interval of
+                        // the trip along with its approximate longitude and latitude
+                        newRouteDuration = routeDuration + stepDuration;
+                        stepModulus = newRouteDuration % 3600;
+
+                        int leftToMakeHour = 3600 - (routeDuration % 3600);
+
+//                        Log.e("Step duration", "" + stepDuration);
+//                        Log.e("Step hour modulus", "" + stepModulus);
+
+                        // If the step duration is greater than the step modulus, then the marker goes here.
+                        if (stepDuration > stepModulus) {
+                            // The marker needs to go the distance from the steps start to end
+                            // times the modulus divided by 3600 (seconds in an hour)
+
+//                            Log.e("Route duration: " + routeDuration + " and adding step with duration", "" + stepDuration);
+
+//                            Log.e("leftToMakeHour", "" + leftToMakeHour);
+
+                            // distance from start to end times (1946/3600)
+
+                            // Create a JSON object from the step
+                            JSONObject step = (JSONObject) jSteps.get(k);
+
+                            // Get the start location of the step
+                            Double stepStartLat;
+                            stepStartLat = ((JSONObject) step.get("start_location")).getDouble("lat");
+                            Double stepStartLng;
+                            stepStartLng = ((JSONObject) step.get("start_location")).getDouble("lng");
+
+                            // Get the end location of the step
+                            Double stepEndLat;
+                            stepEndLat = ((JSONObject) step.get("end_location")).getDouble("lat");
+                            Double stepEndLng;
+                            stepEndLng = ((JSONObject) step.get("end_location")).getDouble("lng");
+
+                            LatLng start = new LatLng(stepStartLat, stepStartLng);
+                            LatLng end = new LatLng(stepEndLat, stepEndLng);
+
+//                            Double hourNum = Math.floor((routeDuration + stepDuration) / 3600);
+//                            int hour = hourNum.intValue();
+
+                            int tempDuration = stepDuration;
+                            int additionalMarkers = 0;
+                            int additionalMarkersDuration;
+                            LatLng marker;
+                            Double percentage;
+
+                            while (tempDuration > 0) {
+
+//                                int hoursTemp = (hourPoints.size() + 1);
+
+                                if(additionalMarkers == 0) {
+                                    percentage = (double) leftToMakeHour / (double) stepDuration;
+                                    marker = getPointBetween(start, end, percentage);
+//                                    Log.e("Adding hour marker", "" + hoursTemp + " for " + (routeDuration + leftToMakeHour));
+                                    if ((tempDuration - leftToMakeHour) < 3600) {
+                                        // there is no way there can be an additional marker so go
+                                        // ahead and break out of the loop now
+                                        tempDuration = 0;
+                                    } else {
+                                        tempDuration = tempDuration - leftToMakeHour;
+                                    }
+                                } else {
+                                    // The time used up so far by the step.
+                                    additionalMarkersDuration = stepDuration - tempDuration + 3600;
+
+                                    percentage = (double) additionalMarkersDuration / (double) stepDuration;
+                                    marker = getPointBetween(start, end, percentage);
+
+//                                    Log.e("Adding hour marker", "" + hoursTemp + " for " + (routeDuration + additionalMarkersDuration));
+                                    tempDuration = tempDuration - 3600;
+
+                                    if (tempDuration < 3600) {
+                                        // break out of the loop now
+                                        tempDuration = 0;
+                                    }
+//                                    Log.e("Left over", "" + tempDuration);
+                                }
+
+                                hourPoints.add(marker);
+                                additionalMarkers++;
+                            }
+                        }
+
+                        // In either case, add the step duration to the route duration
+                        // TODO: Remove this once debugging phase is finished
+                        routeDuration = routeDuration + stepDuration;
                     }
                 }
+                Log.e("Route total duration", "" + routeDuration);
+
                 routes.add(path);
             }
 
--- app/src/main/java/com/bluesierralabs/freewayforecast/routeSelectActivity.java
+++ app/src/main/java/com/bluesierralabs/freewayforecast/routeSelectActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.StrictMode;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -15,25 +14,19 @@ import android.widget.Toast;
 
 import com.bluesierralabs.freewayforecast.Helpers.DirectionsJSONParser;
 import com.bluesierralabs.freewayforecast.Helpers.GMapV2Direction;
-import com.google.android.gms.maps.CameraUpdate;
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
 
 import org.json.JSONObject;
-import org.w3c.dom.Document;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.lang.reflect.Array;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
@@ -47,6 +40,7 @@ public class RouteSelectActivity extends FragmentActivity {
 
     GoogleMap map;
     ArrayList<LatLng> markerPoints;
+    List<LatLng> hourPoints;
     TextView tvDistanceDuration;
 
     LatLng fromPosition = new LatLng(42.3584865, -71.05985749999999);
@@ -115,269 +109,200 @@ public class RouteSelectActivity extends FragmentActivity {
 //        }
     }
 
-        private String getDirectionsUrl(LatLng origin, LatLng dest)
+    private String getDirectionsUrl(LatLng origin, LatLng dest)
+    {
+        // Origin of route
+        String str_origin = "origin=" + origin.latitude + "," + origin.longitude;
+
+        // Destination of route
+        String str_dest = "destination=" + dest.latitude + "," + dest.longitude;
+
+        // Sensor enabled
+        String sensor = "sensor=false";
+
+        // Building the parameters to the web service
+        String parameters = str_origin + "&" + str_dest + "&" + sensor;
+
+        // Output format
+        String output = "json";
+
+        // Building the url to the web service
+        String url = "https://maps.googleapis.com/maps/api/directions/" + output + "?" + parameters;
+
+        return url;
+    }
+
+    /** A method to download json data from url */
+    private String downloadUrl(String strUrl) throws IOException
+    {
+        String data = "";
+        InputStream iStream = null;
+        HttpURLConnection urlConnection = null;
+        try
         {
-            // Origin of route
-            String str_origin = "origin=" + origin.latitude + "," + origin.longitude;
+            URL url = new URL(strUrl);
 
-            // Destination of route
-            String str_dest = "destination=" + dest.latitude + "," + dest.longitude;
+            // Creating an http connection to communicate with url
+            urlConnection = (HttpURLConnection) url.openConnection();
 
-            // Sensor enabled
-            String sensor = "sensor=false";
+            // Connecting to url
+            urlConnection.connect();
 
-            // Building the parameters to the web service
-            String parameters = str_origin + "&" + str_dest + "&" + sensor;
+            // Reading data from url
+            iStream = urlConnection.getInputStream();
 
-            // Output format
-            String output = "json";
+            BufferedReader br = new BufferedReader(new InputStreamReader(iStream));
 
-            // Building the url to the web service
-            String url = "https://maps.googleapis.com/maps/api/directions/" + output + "?" + parameters;
+            StringBuffer sb = new StringBuffer();
 
-            return url;
+            String line = "";
+            while ((line = br.readLine()) != null)
+            {
+                sb.append(line);
+            }
+
+            data = sb.toString();
+
+            br.close();
+
+        } catch (Exception e)
+        {
+            Log.d("Exception while downloading url", e.toString());
+        } finally
+        {
+            iStream.close();
+            urlConnection.disconnect();
         }
+        return data;
+    }
 
-        /** A method to download json data from url */
-        private String downloadUrl(String strUrl) throws IOException
+    // Fetches data from url passed
+    private class DownloadTask extends AsyncTask<String, Void, String>
+    {
+        // Downloading data in non-ui thread
+        @Override
+        protected String doInBackground(String... url)
         {
+
+            // For storing data from web service
             String data = "";
-            InputStream iStream = null;
-            HttpURLConnection urlConnection = null;
+
             try
             {
-                URL url = new URL(strUrl);
-
-                // Creating an http connection to communicate with url
-                urlConnection = (HttpURLConnection) url.openConnection();
-
-                // Connecting to url
-                urlConnection.connect();
-
-                // Reading data from url
-                iStream = urlConnection.getInputStream();
-
-                BufferedReader br = new BufferedReader(new InputStreamReader(iStream));
-
-                StringBuffer sb = new StringBuffer();
-
-                String line = "";
-                while ((line = br.readLine()) != null)
-                {
-                    sb.append(line);
-                }
-
-                data = sb.toString();
-
-                br.close();
-
+                // Fetching the data from web service
+                data = RouteSelectActivity.this.downloadUrl(url[0]);
             } catch (Exception e)
             {
-                Log.d("Exception while downloading url", e.toString());
-            } finally
-            {
-                iStream.close();
-                urlConnection.disconnect();
+                Log.d("Background Task", e.toString());
             }
             return data;
         }
 
-        // Fetches data from url passed
-        private class DownloadTask extends AsyncTask<String, Void, String>
+        // Executes in UI thread, after the execution of
+        // doInBackground()
+        @Override
+        protected void onPostExecute(String result)
         {
-            // Downloading data in non-ui thread
-            @Override
-            protected String doInBackground(String... url)
+            super.onPostExecute(result);
+
+            ParserTask parserTask = new ParserTask();
+
+            // Invokes the thread for parsing the JSON data
+            parserTask.execute(result);
+
+        }
+    }
+
+    /** A class to parse the Google Places in JSON format */
+    private class ParserTask extends AsyncTask<String, Integer, List<List<HashMap<String, String>>>>
+    {
+
+        // Parsing the data in non-ui thread
+        @Override
+        protected List<List<HashMap<String, String>>> doInBackground(String... jsonData)
+        {
+            JSONObject jObject;
+            List<List<HashMap<String, String>>> routes = null;
+
+            try
             {
+                jObject = new JSONObject(jsonData[0]);
+                DirectionsJSONParser parser = new DirectionsJSONParser();
 
-                // For storing data from web service
-                String data = "";
+                // Starts parsing data
+                routes = parser.parse(jObject);
 
-                try
-                {
-                    // Fetching the data from web service
-                    data = RouteSelectActivity.this.downloadUrl(url[0]);
-                } catch (Exception e)
-                {
-                    Log.d("Background Task", e.toString());
-                }
-                return data;
-            }
-
-            // Executes in UI thread, after the execution of
-            // doInBackground()
-            @Override
-            protected void onPostExecute(String result)
+                hourPoints = parser.getHourPoints();
+            } catch (Exception e)
             {
-                super.onPostExecute(result);
-
-                ParserTask parserTask = new ParserTask();
-
-                // Invokes the thread for parsing the JSON data
-                parserTask.execute(result);
-
+                e.printStackTrace();
             }
+            return routes;
         }
 
-        /** A class to parse the Google Places in JSON format */
-        private class ParserTask extends AsyncTask<String, Integer, List<List<HashMap<String, String>>>>
+        // Executes in UI thread, after the parsing process
+        @Override
+        protected void onPostExecute(List<List<HashMap<String, String>>> result)
         {
+            ArrayList<LatLng> points = null;
+            PolylineOptions lineOptions = null;
+            MarkerOptions markerOptions = new MarkerOptions();
+            String distance = "";
+            String duration = "";
 
-            // Parsing the data in non-ui thread
-            @Override
-            protected List<List<HashMap<String, String>>> doInBackground(String... jsonData)
+            if (result.size() < 1)
             {
-                JSONObject jObject;
-                List<List<HashMap<String, String>>> routes = null;
-
-                try
-                {
-                    jObject = new JSONObject(jsonData[0]);
-                    DirectionsJSONParser parser = new DirectionsJSONParser();
-
-                    // Starts parsing data
-                    routes = parser.parse(jObject);
-                } catch (Exception e)
-                {
-                    e.printStackTrace();
-                }
-                return routes;
+                Toast.makeText(RouteSelectActivity.this.getBaseContext(), "No Points", Toast.LENGTH_SHORT).show();
+                return;
             }
 
-            // Executes in UI thread, after the parsing process
-            @Override
-            protected void onPostExecute(List<List<HashMap<String, String>>> result)
+            // Traversing through all the routes
+            for (int i = 0; i < result.size(); i++)
             {
-                ArrayList<LatLng> points = null;
-                PolylineOptions lineOptions = null;
-                MarkerOptions markerOptions = new MarkerOptions();
-                String distance = "";
-                String duration = "";
+                points = new ArrayList<LatLng>();
+                lineOptions = new PolylineOptions();
 
-                if (result.size() < 1)
+                // Fetching i-th route
+                List<HashMap<String, String>> path = result.get(i);
+
+                // Fetching all the points in i-th route
+                for (int j = 0; j < path.size(); j++)
                 {
-                    Toast.makeText(RouteSelectActivity.this.getBaseContext(), "No Points", Toast.LENGTH_SHORT).show();
-                    return;
-                }
+                    HashMap<String, String> point = path.get(j);
 
-                // Traversing through all the routes
-                for (int i = 0; i < result.size(); i++)
-                {
-                    points = new ArrayList<LatLng>();
-                    lineOptions = new PolylineOptions();
-
-                    // Fetching i-th route
-                    List<HashMap<String, String>> path = result.get(i);
-
-                    // Fetching all the points in i-th route
-                    for (int j = 0; j < path.size(); j++)
-                    {
-                        HashMap<String, String> point = path.get(j);
-
-                        if (j == 0)
-                        { // Get distance from the list
-                            distance = point.get("distance");
-                            continue;
-                        } else if (j == 1)
-                        { // Get duration from the list
-                            duration = point.get("duration");
-                            continue;
-                        }
-                        double lat = Double.parseDouble(point.get("lat"));
-                        double lng = Double.parseDouble(point.get("lng"));
-                        LatLng position = new LatLng(lat, lng);
-                        points.add(position);
+                    if (j == 0)
+                    { // Get distance from the list
+                        distance = point.get("distance");
+                        continue;
+                    } else if (j == 1)
+                    { // Get duration from the list
+                        duration = point.get("duration");
+                        continue;
                     }
-
-                    // Adding all the points in the route to LineOptions
-                    lineOptions.addAll(points);
-                    lineOptions.width(2);
-                    lineOptions.color(Color.RED);
+                    double lat = Double.parseDouble(point.get("lat"));
+                    double lng = Double.parseDouble(point.get("lng"));
+                    LatLng position = new LatLng(lat, lng);
+                    points.add(position);
                 }
 
-                //RouteSelectActivity.this.tvDistanceDuration.setText("Distance:" + distance + ", Duration:" + duration);
-
-                // Drawing polyline in the Google Map for the i-th route
-                RouteSelectActivity.this.map.addPolyline(lineOptions);
+                // Adding all the points in the route to LineOptions
+                lineOptions.addAll(points);
+                lineOptions.width(5);
+                lineOptions.color(Color.RED);
             }
-        }
 
-//        if (android.os.Build.VERSION.SDK_INT > 9) {
-//            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-//            StrictMode.setThreadPolicy(policy);
-//        }
-//
-//        // Map direction object
-//        md = new GMapV2Direction();
-//
-//        // Map fragment
-//        mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
-//
-//        // Set the trip start and end markers
-//        mMap.addMarker(new MarkerOptions().position(fromPosition).title("Start"));
-//        mMap.addMarker(new MarkerOptions().position(toPosition).title("End"));
-//
-//        // Set up bounds for the camera view of the map
-//        LatLngBounds.Builder builder = new LatLngBounds.Builder();
-//        builder.include(fromPosition);
-//        builder.include(toPosition);
-//        LatLngBounds bounds = builder.build();
-//
-//        // Change the padding as per needed
-//        CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, 25,25,5);
-//        mMap.animateCamera(cu);
-//
-//        Document doc = md.getDocument(fromPosition, toPosition, GMapV2Direction.MODE_DRIVING);
-//        int duration = md.getDurationValue(doc);
-//        String distance = md.getDistanceText(doc);
-//        String start_address = md.getStartAddress(doc);
-//        String copy_right = md.getCopyRights(doc);
-//
-////        ArrayList<LatLng> directionPoint = md.getDirection(doc);
-//        ArrayList<ArrayList> allRoutes = md.getDirection(doc);
-//
-//        ArrayList<String> allSummaries = md.getSummaries(doc);
-//
-//        for(int i=0; i < allRoutes.size(); i++) {
-//            PolylineOptions rectLine = new PolylineOptions();
-//
-//            if (i==0) {
-//                rectLine.width(3).color(Color.RED);
-//            } else if (i==1) {
-//                rectLine.width(3).color(Color.GREEN);
-//            } else {
-//                rectLine.width(3).color(Color.MAGENTA);
-//            }
-//
-//            //PolylineOptions rectLine = new PolylineOptions().width(3).color(Color.GREEN);
-//
-//
-//            ArrayList<LatLng> routeDirections = allRoutes.get(i);
-//
-//            Log.e("Route summary", allSummaries.get(i));
-//            Log.e("RouteSelectActivity", "got route # " + i);
-//            Log.e("RouteSelectActivity", "which has " + routeDirections.size() + " steps");
-//
-//            if (i==0) {
-//                for (int j = 0; j < routeDirections.size(); j++) {
-//                    rectLine.add(routeDirections.get(j));
-//                }
-//            }
-//
-//            mMap.addPolyline(rectLine);
-//
-//            rectLine = null;
-//        }
-//
-////        PolylineOptions rectLine = new PolylineOptions().width(5).color(Color.RED);
-////
-////        Log.e("RouteSelectActivity", "direction points: " + directionPoint.size());
-////
-////        for (int i = 0; i < directionPoint.size(); i++) {
-////            rectLine.add(directionPoint.get(i));
-////        }
-////
-////        mMap.addPolyline(rectLine);
+            //RouteSelectActivity.this.tvDistanceDuration.setText("Distance:" + distance + ", Duration:" + duration);
+
+            Log.e("Trying to add hour markers", "" + hourPoints.size());
+            for (int i = 0; i < hourPoints.size(); i++) {
+                map.addMarker(new MarkerOptions().position(hourPoints.get(i)));
+            }
+
+            // Drawing polyline in the Google Map for the i-th route
+            RouteSelectActivity.this.map.addPolyline(lineOptions);
+        }
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
